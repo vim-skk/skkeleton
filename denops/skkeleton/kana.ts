a8b51edc166d5042330e8cb93d290b0cf91a45de
kuuote <znmxodq1@gmail.com>
2021-08-22 02:17:43 +0900

    Add KanaTable Manager

--- denops/skkeleton/kana.ts
+++ denops/skkeleton/kana.ts
@@ -1,1 +1,20 @@
+import type { KanaTable } from "./kana/type.ts";
 import { romToHira } from "./kana/rom_hira.ts";
+
+const tables: Record<string, KanaTable> = {
+  "romToHira": romToHira,
+};
+
+let currentTable = "romToHira";
+
+export function setCurrentKanaTable(name: string) {
+  currentTable = name;
+}
+
+export function getKanaTable(name = currentTable): KanaTable {
+  const table = tables[name];
+  if (!table) {
+    throw new Error(`undefined table: ${name}`);
+  }
+  return table;
+}
