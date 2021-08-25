14420010888da047f941f675c42358a9298465d3
kuuote <znmxodq1@gmail.com>
2021-08-25 14:10:08 +0900

    update deps

--- denops/skkeleton/deps.ts
+++ denops/skkeleton/deps.ts
@@ -1,10 +1,11 @@
-export * as autocmd from "https://deno.land/x/denops_std@v1.5.0/autocmd/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v1.5.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.5.0/variable/mod.ts";
-export * from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
-export type { Denops } from "https://deno.land/x/denops_std@v1.5.0/mod.ts";
+export * as anonymous from "https://deno.land/x/denops_std@v1.7.4/anonymous/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.7.4/autocmd/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v1.7.4/function/mod.ts";
 export {
   batch,
   execute,
-} from "https://deno.land/x/denops_std@v1.5.0/helper/mod.ts";
+} from "https://deno.land/x/denops_std@v1.7.4/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.7.4/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.7.4/variable/mod.ts";
+export * from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
 export { ensureString } from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
