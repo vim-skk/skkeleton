51a03e171cbdbc350d6f205b6f7736ce0ecd9aa4
kuuote <znmxodq1@gmail.com>
2021-08-27 12:14:03 +0900

    Add configuration validator

--- denops/skkeleton/config.ts
+++ denops/skkeleton/config.ts
@@ -1,7 +1,50 @@
+import { ensureBoolean, ensureNumber, ensureString } from "./deps.ts";
+
 export const config = {
+  globalJisyo: "/usr/share/skk/SKK-JISYO.L",
+  globalJisyoEncoding: "euc-jp",
+  immediatelyJisyoRW: true,
   selectCandidateKeys: "asdfjkl",
   setUndoPoint: true,
   showCandidatesCount: 4,
   usePopup: true,
-  immediatelyJisyoRW: true,
+  userJisyo: Deno.env.get("HOME") + "/.skkeleton",
 };
+
+type Validators = {
+  [P in keyof typeof config]: (x: unknown) => asserts x is typeof config[P];
+};
+
+const validators: Validators = {
+  globalJisyo: ensureString,
+  globalJisyoEncoding: ensureString,
+  immediatelyJisyoRW: ensureBoolean,
+  selectCandidateKeys: (x): asserts x is string => {
+    ensureString(x);
+    if (x.length !== 7) {
+      throw TypeError("selectCandidateKeys !== 7");
+    }
+  },
+  setUndoPoint: ensureBoolean,
+  showCandidatesCount: ensureNumber,
+  usePopup: ensureBoolean,
+  userJisyo: ensureString,
+};
+
+export function setConfig(newConfig: Record<string, unknown>) {
+  const cfg = config as Record<string, unknown>;
+  const val = validators as Record<string, (x: unknown) => void>;
+  for (const k in newConfig) {
+    try {
+      if (val[k]) {
+        val[k](newConfig[k]);
+        cfg[k] = newConfig[k];
+      } else {
+        throw TypeError(`unknown option: ${k}`);
+      }
+    } catch (e) {
+      console.error("Illegal option detected");
+      console.error(e.toString());
+    }
+  }
+}
