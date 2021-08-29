6a88be78a158de7909b7f1fd6cbda7b6e57461ce
kuuote <znmxodq1@gmail.com>
2021-08-29 23:17:17 +0900

    Add test for eggLikeNewline

---
+++ denops/skkeleton/config_test.ts
@@ -0,0 +1,25 @@
+import { assertEquals } from "https://deno.land/std@0.103.0/testing/asserts.ts";
+import { config } from "./config.ts";
+import { Context } from "./context.ts";
+import { dispatch } from "./function/testutil.ts";
+import { currentLibrary } from "./jisyo.ts";
+
+const defaultConfig = { ...config };
+
+const lib = currentLibrary.get();
+lib.registerCandidate("okurinasi", "あ", "あ");
+
+Deno.test({
+  name: "egg like newline",
+  async fn() {
+    Object.assign(config, defaultConfig);
+    const context = new Context();
+    // normal
+    await dispatch(context, "A \nA\n");
+    assertEquals(context.preEdit.output(""), "あ\nあ\n");
+    // egg like
+    config.eggLikeNewline = true;
+    await dispatch(context, "A \nA\n");
+    assertEquals(context.preEdit.output(""), "ああ");
+  },
+});
