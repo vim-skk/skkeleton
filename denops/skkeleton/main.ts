2d8b506add7dc4cc0a93e340cfca0d5b325cde5c
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-10-09 18:15:40 +0900

    fmt

--- denops/skkeleton/main.ts
+++ denops/skkeleton/main.ts
@@ -218,7 +218,7 @@ export async function main(denops: Denops) {
       ensureString(kana);
       ensureString(word);
       currentLibrary.get().registerCandidate("okurinasi", kana, word);
-      return Promise.resolve()
+      return Promise.resolve();
     },
   };
   if (config.debug) {
