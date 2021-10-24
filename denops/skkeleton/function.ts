376d97afafce92c91e1b4ec696d0715b37c37f9a
kuuote <znmxodq1@gmail.com>
2021-10-24 21:25:30 +0900

    Add map for hankatakana

--- denops/skkeleton/function.ts
+++ denops/skkeleton/function.ts
@@ -8,7 +8,7 @@ import {
   henkanInput,
 } from "./function/henkan.ts";
 import { deleteChar, henkanPoint, kakuteiFeed } from "./function/input.ts";
-import { katakana } from "./function/mode.ts";
+import { hankatakana, katakana } from "./function/mode.ts";
 import { Cell } from "./util.ts";
 
 export type Func = (
@@ -35,4 +35,5 @@ export const functions = new Cell<Record<string, Func>>(() => ({
   deleteChar,
   // mode
   katakana,
+  hankatakana,
 }));
