f1b8e7e406ae89be32bbe40279d253d5c2e06aa8
delphinus <me@delphinus.dev>
2021-11-27 23:55:32 +0900

    Do not set zenkaku() to the global keyMap
    Because <s-l> should work only in rom Kanatable. Without this, <s-l>
    does not work in zenkaku mode...it should input `L`.

--- denops/skkeleton/keymap.ts
+++ denops/skkeleton/keymap.ts
@@ -9,7 +9,7 @@ import {
   henkanInput,
 } from "./function/henkan.ts";
 import { deleteChar, kanaInput } from "./function/input.ts";
-import { hankatakana, zenkaku } from "./function/mode.ts";
+import { hankatakana } from "./function/mode.ts";
 import { keyToNotation, notationToKey } from "./notation.ts";
 
 type KeyMap = {
@@ -27,7 +27,6 @@ const input: KeyMap = {
     "<esc>": escape,
     "<nl>": kakutei,
     "<c-q>": hankatakana,
-    "<s-l>": zenkaku,
   },
 };
 
