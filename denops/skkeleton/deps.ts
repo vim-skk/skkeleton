bd665b7271167e9a9095fd3086f0a577f06f22a5
kuuote <znmxodq1@gmail.com>
2022-02-24 04:01:22 +0900

    chore: update deps

--- denops/skkeleton/deps.ts
+++ denops/skkeleton/deps.ts
@@ -1,11 +1,11 @@
-export * as anonymous from "https://deno.land/x/denops_std@v2.1.2/anonymous/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v2.1.2/autocmd/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v2.1.2/function/mod.ts";
+export * as anonymous from "https://deno.land/x/denops_std@v3.1.3/anonymous/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.1.3/autocmd/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.1.3/function/mod.ts";
 export {
   batch,
   gather,
-} from "https://deno.land/x/denops_std@v2.1.2/batch/mod.ts";
-export type { Denops } from "https://deno.land/x/denops_std@v2.1.2/mod.ts";
-export * as mapping from "https://deno.land/x/denops_std@v2.1.2/mapping/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v2.1.2/variable/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v2.1.2/option/mod.ts";
+} from "https://deno.land/x/denops_std@v3.1.3/batch/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.1.3/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v3.1.3/mapping/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.1.3/variable/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v3.1.3/option/mod.ts";
--- denops/skkeleton/deps/denops_test.ts
+++ denops/skkeleton/deps/denops_test.ts
@@ -1,1 +1,1 @@
-export { test } from "https://deno.land/x/denops_core@v2.0.3/test/mod.ts";
+export { test } from "https://deno.land/x/denops_core@v3.0.0/test/mod.ts";
