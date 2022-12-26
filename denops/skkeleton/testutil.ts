1d0fc2d7befa59d9d1808351f29e457ce161669c
kuuote <znmxodq1@gmail.com>
2022-12-26 14:05:00 +0900

    test: fix running on Windows

--- denops/skkeleton/testutil.ts
+++ denops/skkeleton/testutil.ts
@@ -6,7 +6,7 @@ import { currentContext } from "./store.ts";
 export async function initDenops(denops: Denops): Promise<void> {
   const cfile = fromFileUrl(new URL(import.meta.url));
   const path = cfile.slice(0, cfile.lastIndexOf("denops"));
-  await denops.cmd(`set runtimepath^=${path}`);
+  await denops.cmd(`set runtimepath^=${path.replaceAll("\\", "/")}`);
   await denops.cmd("source " + path + "plugin/skkeleton.vim");
   await main(denops);
   await autocmd.emit(denops, "User", "DenopsPluginPost:skkeleton", {
