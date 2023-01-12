805c5f625c8d6e6c5442e244cca70db10cc7f0a2
kuuote <znmxodq1@gmail.com>
2023-01-13 01:50:45 +0900

    test: update test runner

--- denops/skkeleton/deps/denops_test.ts
+++ denops/skkeleton/deps/denops_test.ts
@@ -1,1 +1,1 @@
-export { test } from "https://raw.githubusercontent.com/kuuote/deno-denops-test/4e0fe76699ad77471344ff9e6d66672ec12394b9/denops_test/tester.ts";
+export { test } from "https://deno.land/x/denops_test@v1.1.0/tester.ts";
