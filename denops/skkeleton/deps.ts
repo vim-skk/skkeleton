8d4e3273e0b6f2ba92695e22ac68ad1155fd7795
uga-rosa <uga6603@gmail.com>
2023-05-29 13:48:11 +0900

    Remove function removed in denops v5

--- denops/skkeleton/deps.ts
+++ denops/skkeleton/deps.ts
@@ -1,9 +1,6 @@
 export * as autocmd from "https://deno.land/x/denops_std@v5.0.0/autocmd/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
-export {
-  batch,
-  gather,
-} from "https://deno.land/x/denops_std@v5.0.0/batch/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v5.0.0/batch/mod.ts";
 export type { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
 export * as mapping from "https://deno.land/x/denops_std@v5.0.0/mapping/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v5.0.0/variable/mod.ts";
