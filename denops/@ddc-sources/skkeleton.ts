1a90251da701fa1af493b11df4518f17c273ccfd
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-08-18 10:00:04 +0900

    Fix abbrPrefix for neovim

--- denops/@ddc-sources/skkeleton.ts
+++ denops/@ddc-sources/skkeleton.ts
@@ -6,10 +6,8 @@ import {
   OnCompleteDoneArguments,
 } from "../skkeleton/deps/ddc/source.ts";
 import { DdcGatherItems, Previewer } from "../skkeleton/deps/ddc/types.ts";
-import { fn } from "../skkeleton/deps.ts";
 import type {
   CompletionData,
-  ConfigOptions,
   RankData,
 } from "../skkeleton/types.ts";
 
@@ -47,12 +45,9 @@ export class Source extends BaseSource<Params> {
     // グローバル辞書由来の候補はユーザー辞書の末尾より配置する
     // 辞書順に並べるため先頭から順に負の方向にランクを振っていく
     let globalRank = -1;
-    const config = await args.denops.call(
-      "skkeleton#get_config",
-    ) as ConfigOptions;
-    const abbrPrefix = " ".repeat(
-      await fn.strwidth(args.denops, config.markerHenkan),
-    );
+    // NOTE: neovim の場合、floatwin 境界にマルチバイトがあると表示が崩れるバグ
+    // がある
+    const abbrPrefix = args.denops.meta.host === "nvim" ? " " : "";
     const ddcCandidates = candidates.flatMap((e) => {
       return e[1].map((word) => ({
         word: word.replace(/;.*$/, ""),
