846f29ba0735e4400945cc2cccfec15e3cf18d70
atusy <30277794+atusy@users.noreply.github.com>
2024-06-08 18:22:07 +0900

    fix: cares only > and ignore ＞
    No longer needed because it only mattered in my PoC

--- denops/skkeleton/candidate.ts
+++ denops/skkeleton/candidate.ts
@@ -4,9 +4,9 @@ export function modifyCandidate(candidate: string, affix?: AffixType) {
   const candidateStrip = candidate?.replace(/;.*/, "");
 
   if (affix === "prefix") {
-    return candidateStrip?.replace(/[>＞]$/, "");
+    return candidateStrip?.replace(/>$/, "");
   } else if (affix === "suffix") {
-    return candidateStrip?.replace(/^[>＞]/, "");
+    return candidateStrip?.replace(/^>/, "");
   } else {
     return candidateStrip;
   }
