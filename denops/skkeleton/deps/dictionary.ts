7c33a16fd9f2cbec8f5ec2daeece99b073ef2d91
kuuote <znmxodq1@gmail.com>
2024-08-23 12:53:32 +0900

    fix: change assert => with for deprecation warning

--- denops/skkeleton/deps/dictionary.ts
+++ denops/skkeleton/deps/dictionary.ts
@@ -1,4 +1,4 @@
 export * as yaml from "jsr:@std/yaml@1.0.0";
 export * as msgpack from "https://esm.sh/@msgpack/msgpack@3.0.0-beta2";
 export { default as jsonschema } from "https://esm.sh/jsonschema@1.4.1";
-export { default as jisyoschema } from "https://cdn.jsdelivr.net/gh/skk-dict/jisyo/schema/jisyo.schema.v0.0.0.json" assert { type: "json" };
+export { default as jisyoschema } from "https://cdn.jsdelivr.net/gh/skk-dict/jisyo/schema/jisyo.schema.v0.0.0.json" with { type: "json" };
